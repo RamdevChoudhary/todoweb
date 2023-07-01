e7cabee7e85b2be1056d3fb8dd9d854df1180484
Ramdev Choudhary <120701610+kimunjong@users.noreply.github.com>
2023-07-01 16:05:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+#-------------------------------------Overview-------------------------------------#
+# This code implements a basic Todo application using Flask, SQLAlchemy, and SQLite.
+# The application allows users to add, update, and delete tasks in a todo list. The 
+# tasks are stored in a SQLite database using SQLAlchemy as an ORM 
+# (Object-Relational Mapping) tool.
+
+# **Dependencies
+# The following libraries are required to run the code:#
+
+                 # **Flask** #
+              # **SQLAlchemy** #
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+# Flask: The Flask library is imported to create the Flask application instance.
+# render_template: This function is imported to render HTML templates.
+# request: This module is imported to handle HTTP requests.
+# redirect: This function is imported to redirect the user to a different URL.
+# Flask_SQLAlchemy: This extension is imported to connect the Flask application with the SQLite database.
+# datetime: This module is imported to handle timestamps for the tasks.
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tudo.db"
@@ -12,10 +30,16 @@ class Todo(db.Model):
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
+    ### Database Model ###
+    # The database model is defined using SQLAlchemy in the following class:
+    
     def __repr__(self) -> str:
         return f"{self.sno}  -  {self.title}"
 
+# Routes and Functionality
+# Route: "/"
+# This route handles the home page and allows users to add new tasks and 
+# view existing tasks.
 @app.route("/", methods=['GET', 'POST'])
 def helloworld():
     if request.method=="POST":
